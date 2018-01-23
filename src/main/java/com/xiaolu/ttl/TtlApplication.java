434b1dc932145c44341271368a1fd823c6ff205e
apple <xl1346@nyu.edu>
2018-01-22 20:03:31 -0800

    change url4

--- src/main/java/com/xiaolu/ttl/TtlApplication.java
+++ src/main/java/com/xiaolu/ttl/TtlApplication.java
@@ -19,7 +19,7 @@ public class TtlApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(*);
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
